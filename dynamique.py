79ee9e569d101379f9a55008042321dd357243a3
Rayen <rayenhamadi48@gmail.com>
2025-07-29 09:09:16 +0200

    setup tooling

--- dynamique.py
+++ dynamique.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # Exemple : dynamique à deux bacs interconnectés
 def flow_two_tanks(x, t, params):
     """
@@ -9,13 +10,12 @@ def flow_two_tanks(x, t, params):
     :param params: dictionnaire des paramètres {A1, A2, k12, k2, q_in}
     :return: dérivées des états
     """
-    A1, A2 = params['A1'], params['A2']
-    k12, k2 = params['k12'], params['k2']
-    q_in = params['q_in']
+    A1, A2 = params["A1"], params["A2"]
+    k12, k2 = params["k12"], params["k2"]
+    q_in = params["q_in"]
 
     h1, h2 = x
     q12 = k12 * np.sqrt(abs(h1 - h2))
     qout = 5 * k2 * np.sqrt(abs(h2))
 
     return [(q_in - q12) / A1, (q12 - qout) / A2]
-
